6f937afa45fb1306268e255865e5c430792b4815
dhirajkrgupta <157622113+dhirajkrgupta@users.noreply.github.com>
2024-06-18 16:46:07 +0530

    state persistance

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { useState } from "react";
-import TodoList from "./components/TodoList"
-import Todoinput from "./components/Todoinput"
+import { useEffect, useState } from "react";
+import TodoList from "./components/TodoList";
+import Todoinput from "./components/Todoinput";
 
 function App() {
-  const [Tasks, setTasks] = useState([]);
-  const [inputTask, setInputTask] = useState('');
+  const [Tasks, setTasks] = useState(loadStateFromLocalStorage||{});
+  const [inputTask, setInputTask] = useState([]);
 
   function addTask(newTask) {
     const newTasks = [...Tasks, newTask];
     setTasks(newTasks);
   }
   function removeTask(index) {
-    const newTasks = Tasks.filter((task,taskidx) => {
+    const newTasks = Tasks.filter((task, taskidx) => {
       return taskidx != index;
-    })
+    });
     setTasks(newTasks);
   }
   function editTask(index) {
@@ -22,34 +22,49 @@ function App() {
     removeTask(index);
   }
 
-  window.addEventListener('load', () => {
-   
-    const taskinput = document.querySelector('.taskinput');
-    const tasklist= document.querySelector('.tasklist');
-    const inputfield = document.querySelector('.inputfield');
-    const taskcard = document.querySelector('.taskcard');
+  window.addEventListener("load", () => {
+    const taskinput = document.querySelector(".taskinput");
+    const tasklist = document.querySelector(".tasklist");
+    const inputfield = document.querySelector(".inputfield");
+    const taskcard = document.querySelector(".taskcard");
 
     function matchWidth() {
-      const widthA = taskinput.offsetWidth; 
-      const widthB = inputfield.offsetWidth; 
-      tasklist.style.width = widthA + 'px'; 
-      taskcard.style.width = widthB + 'px'; 
+      const widthA = taskinput.offsetWidth;
+      const widthB = inputfield.offsetWidth;
+      tasklist.style.width = widthA + "px";
+      taskcard.style.width = widthB + "px";
     }
 
     matchWidth();
 
-
-    window.addEventListener('resize', matchWidth);
+    window.addEventListener("resize", matchWidth);
   });
 
+  function storeStateToLocalStorage(st) {
+    localStorage.setItem("appState", JSON.stringify(st));
+  }
+  function loadStateFromLocalStorage() {
+    const st = localStorage.getItem("appState");
+    return st ? JSON.parse(st) : undefined;
+  }
+
+  useEffect(
+    () => {
+      storeStateToLocalStorage(Tasks);
+    },
+    [Tasks]
+  );
+
   return (
-   
-      <main>
-        <Todoinput addTask={addTask} inputTask={inputTask} setInputTask={setInputTask} />
-        <TodoList Tasks={Tasks} removeTask={removeTask} editTask={editTask } />
-      </main>
-    
-  )
+    <main>
+      <Todoinput
+        addTask={addTask}
+        inputTask={inputTask}
+        setInputTask={setInputTask}
+      />
+      <TodoList Tasks={Tasks} removeTask={removeTask} editTask={editTask} />
+    </main>
+  );
 }
 
-export default App
+export default App;
