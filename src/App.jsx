a3ac34b0f3068a941d119e675b81b9225e20708d
dhirajkrgupta <157622113+dhirajkrgupta@users.noreply.github.com>
2025-07-02 16:14:18 +0530

    text selection disabled

--- src/App.jsx
+++ src/App.jsx
@@ -74,7 +74,7 @@ function App() {
 
   return (
     <TodoContextProvider value={{Tasks,addTask,removeTask,editTask,finishTask,unfinishTask}}>
-      <div className="container min-h-screen overflow-y-auto min-w-max mx-auto    text-white p-4 ">
+      <div className="container min-h-screen overflow-y-auto min-w-max mx-auto    text-white p-4 select-none ">
         <h1 className="text-[#60d9b8] font-bold text-center text-3xl mb-2">Your To Do List</h1>
         <div className="  min-w-max h-fit  p-4">
           <div className="flex mb-4">
